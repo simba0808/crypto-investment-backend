0331139f59e653ada39175109fcb13bfa77a0f38
simba <simba0808@gmail.com>
2023-10-31 02:14:25 +0900

    update admin panel

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
-import userBalance from './routes/userBalance.js'
-import treeRoutes from './routes/treeRoutes.js'
+import userBalance from './routes/userBalance.js';
+import treeRoutes from './routes/treeRoutes.js';
+import historyRoutes from './routes/historyRoutes.js';
+
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -22,6 +24,7 @@ app.use(cookieParser());
 app.use('/api/users', userRoutes);
 app.use('/api/balance', userBalance);
 app.use('/api/tree', treeRoutes);
+app.use('/api/history', historyRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
