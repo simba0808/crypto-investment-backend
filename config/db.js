73f6664b97303fd6fd590634456d7dd95dbbe4f3
simba <simba0808@gmail.com>
2023-11-01 20:47:58 +0900

    db reverse to env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://jumaquiho86:plxtFS8khtmuqRmI@cluster0.wgeee1j.mongodb.net/");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
