818a46648e7caee7dc7c8148d4a9270d8b44e85a
simba <153499114+simba0808@users.noreply.github.com>
2024-05-27 04:09:53 +1000

    fix: cors

--- controllers/balanceController.js
+++ controllers/balanceController.js
@@ -3,7 +3,6 @@ import User from '../models/userModel.js';
 import Tree from '../models/userTree.js';
 import History from '../models/historyModel.js';
 import Invoice from '../models/invoiceModel.js';
-import History from '../models/historyModel.js'
 import { showBalance } from './historyController.js';
 import axios from 'axios';
 import speakeasy from 'speakeasy';
@@ -296,15 +295,11 @@ const generateInvoice = asyncHandler(async (req, res) => {
 
     try {
       const response = await axios.post(process.env.NOWPAY_SERVER + '/sub-partner/payment', pay_info);
-<<<<<<< HEAD
       console.log(response.data.result);
       const invoice = await Invoice.create({
         sub_partner_id: user.sub_partner_id,
         invoice_id: response.data.result.invoice_id,
       });
-
-=======
->>>>>>> origin/main
       res.status(200).send(response.data.result);
     } catch (err) {
       console.log(err);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
@@ -18,6 +19,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: 'https://investworld.vercel.app/',
+  optionsSuccessStatus: 200,
+}));
 
 app.use(cookieParser());
 
